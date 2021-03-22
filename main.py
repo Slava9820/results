e651c8b1593a17b6ed13f6e679a8a6e244a9de74
Slava9820 <73226990+Slava9820@users.noreply.github.com>
2021-03-22 14:18:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import json
 import matplotlib.pylab as plt
+import os.path
 
 data = {}
 data['data'] = []
@@ -11,9 +12,9 @@ x = np.arange(-100,100,0.01)
 A = 1.25313
 y = []
 for i in x:
-    b = math.sin((i * i - A * A))
+    b = math.sin((i * i - A))
     c = math.cos(b) ** 2
-    n = 0.001 * (i * i + A * A)
+    n = 0.001 * (i * i + A)
     function = 0.5 + ( c - 0.5) / (1 + n)
     y.append(function)
     data['data'].append({
@@ -21,8 +22,15 @@ for i in x:
         'y': '{:.2f}'.format(function)
     })
 
-with open('res.json', 'w') as outfile:
-    json.dump(data, outfile)
 plt.grid()
 plt.plot(x,y)
 plt.show()
+dirname = os.path.dirname('results.json')
+filename = os.path.join(dirname, 'results')
+if os.path.exists(filename) == False:
+    os.mkdir(filename)
+filename = os.path.join(filename, 'results.json')
+
+with open(filename, 'w') as outfile:
+    json.dump(data, outfile)
+
