b676232314303c07b5cca7472f64ca60d60874b0
Slava9820 <73226990+Slava9820@users.noreply.github.com>
2021-03-31 10:02:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,26 +11,25 @@ data['data'] = []
 x = np.arange(-100,100,0.01)
 A = 1.25313
 y = []
-for i in x:
-    b = math.sin((i * i - A))
+def asd(i):
+    b = math.sin((i * i - A*A))
     c = math.cos(b) ** 2
-    n = 0.001 * (i * i + A)
-    function = 0.5 + ( c - 0.5) / (1 + n)
+    n = 0.001 * (i * i + A*A)
+    function = 0.5 + (c - 0.5) / (1 + n)
     y.append(function)
-    data['data'].append({
-        'x': '{:.2f}'.format(i),
-        'y': '{:.2f}'.format(function)
-    })
+    return function
+
+[data['data'].append({'x': '{:.2f}'.format(i),'y': '{:.2f}'.format(asd(i))}) for i in x]
 
 plt.grid()
 plt.plot(x,y)
 plt.show()
-dirname = os.path.dirname('results.json')
-filename = os.path.join(dirname, 'results')
-if os.path.exists(filename) == False:
+filename = os.path.join('results')
+if not os.path.exists(filename) == True:
     os.mkdir(filename)
 filename = os.path.join(filename, 'results.json')
 
 with open(filename, 'w') as outfile:
     json.dump(data, outfile)
+
 
